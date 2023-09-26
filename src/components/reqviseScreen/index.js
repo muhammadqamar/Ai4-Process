b8f17641010a59140fcf14dff440693e55bfb037
muhammadqamar <muhammadqamar111@gmail.com>
2023-09-26 15:22:01 +0500

    add drag_Drop

--- src/components/reqviseScreen/index.js
+++ src/components/reqviseScreen/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import Revqvise from "../../assets/images/reqviseLogo.svg"
-import Button from "../../utils/Button/index"
-import Footer from "../Layout/Footer/footer"
+import React from "react";
+import Revqvise from "../../assets/images/reqviseLogo.svg";
+import Button from "../../utils/Button/index";
+import Footer from "../Layout/Footer/footer";
+import DragDrop from "../common/dragDrop";
 
 const Index = () => {
   return (
-    <div className='w-full min-h-screen bg-Purple-900 pt-[100px]'>
-      <div className='h-[529px] border-y-4 border-solid'>
-        <div className='flex flex-col items-center text-White justify-center gap-[19px] pt-[126px]'>
+    <div className="w-full min-h-screen bg-Purple-900 pt-[100px]">
+      <div className="h-[529px] border-y-4 border-solid">
+        <div className="flex flex-col items-center text-White justify-center gap-[19px] pt-[126px]">
           <img src={Revqvise} />
-          <p className='h2 pl-[91px] pr-[41px]'>Reqvise is a tool that uses natural language processing to help system requirement engineers write requirements that are clear, consistent, and can be tested.</p>
+          <p className="h2 pl-[91px] pr-[41px]">
+            Reqvise is a tool that uses natural language processing to help
+            system requirement engineers write requirements that are clear,
+            consistent, and can be tested.
+          </p>
         </div>
-        <div>
-          {/* <Button /> */}
-        </div>
+        <div>{/* <Button /> */}</div>
       </div>
+      <DragDrop />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,20 @@ module.exports = {
         White: "rgba(254, 254, 254, 1)",
         "White-200": "rgba(228, 226, 226, 1)",
         "White-300": "rgba(196, 196, 196, 0.60)",
+        "White-500": "rgba(255, 255, 255, 1)",
         Grey: "#919094",
         "Gray-100": "#C4C4C4",
+        "Grey-400": "rgba(255, 255, 255, 0.6)",
         "Grey-500": "rgba(145, 144, 148, 1)",
         "Grey-700": "rgba(44, 45, 58, 0.6)",
+        "Grey-800": "rgba(217, 217, 217, 0.10)",
         Black: "#000000",
+        "Black-800": "rgba(8, 9, 12, 1)",
         "Purple-500": "#1E1F28",
         "Purple-900": "rgba(8, 9, 12, 1)",
         "Grey-200": "#D9D9D9",
         "Mist-500": "rgba(69, 217, 252, 1)",
+        "Mist-600": "rgba(85, 204, 204, 1)",
       },
 
       boxShadow: {
@@ -23,7 +28,8 @@ module.exports = {
         "medium-shadow": "0px 0px 0px 1px rgba(0, 0, 0, 0.34)",
       },
       backgroundImage: {
-        'gradient-100': "linear-gradient(90deg, #45D9FC 17.14%, #5846F9 94.72%)"
+        "gradient-100":
+          "linear-gradient(90deg, #45D9FC 17.14%, #5846F9 94.72%)",
       },
 
       screens: {
