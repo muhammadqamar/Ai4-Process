3ad2ab5e3427ba5aa28561a45c66d16c4acec7ee
muhammadqamar <muhammadqamar111@gmail.com>
2023-09-26 15:00:13 +0500

    get pull

--- src/components/Layout/Header/header.js
+++ src/components/Layout/Header/header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Reqvise from "../../../assets/images/reqviseLogo2.svg"
+import { Button } from "../../../utils/Button"
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
+    <div className="w-full mx-auto">
+      <img src={Reqvise} alt="reqviseLogo" className="ml-[44px]" />
+      <div>
+        sdfsd
+        {/* <Button text="Support" />
+        <Button text="Logout" /> */}
+      </div>
     </div>
   );
 };
