61b005949a898f152da504e3296b49520acca3e0
muhammadqamar <muhammadqamar111@gmail.com>
2023-09-26 12:49:09 +0500

    main section

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import AiBG from "../../assets/images/Ai-bg.png";
+import LibersiftLogo from "../../assets/images/libersift-logo.svg";
+import ReqvuiseLogo from "../../assets/images/reqvuise-logo.svg";
 
 const Home = () => {
   return (
     <div
-      className="w-full min-h-screen "
+      className="w-full min-h-screen py-[94px]"
       style={{
         backgroundImage: `url(${AiBG})`,
         backgroundPosition: "center",
@@ -13,8 +15,8 @@ const Home = () => {
         backgroundSize: "cover",
       }}
     >
-      <div className="max-w-[913px] h-full pt-[94px] mx-auto">
-        <div className="w-full text-center text-[#fff] mb-4">
+      <div className="max-w-[913px] h-full  mx-auto text-[#fff]">
+        <div className="w-full text-center  mb-4">
           <p className="text-[20px] font-light leading-normal">Welcome to</p>
           <h1 className="text-[45px] font-bold leading-normal mb-[58px]">
             Ai4Process
@@ -24,7 +26,34 @@ const Home = () => {
           </p>
         </div>
 
-        <div className="w-full flex items-center  justify-between gap-6"></div>
+        <div className="w-full flex items-center  justify-between gap-6">
+          <div className="w-[325px] cursor-pointer">
+            <img
+              src={ReqvuiseLogo}
+              alt="Libersift"
+              className="w-fit h-auto object-contain mx-auto mb-[35px]"
+            />
+            <p className="text-[20px] font-light leading-normal">
+              Reqvise is a tool that uses natural language processing to help
+              system requirement engineers write requirements that are clear,
+              consistent, and can be tested.
+            </p>
+          </div>
+          <div className="w-1 h-[383px] bg-[#fff]" />
+          <div className="w-[368px]">
+            <img
+              src={LibersiftLogo}
+              alt="Libersift"
+              className="w-fit h-auto object-contain mx-auto mb-[35px]"
+            />
+            <p className="text-[20px] font-light leading-normal">
+              Libersift is AI-powered search tool that revolutionizes the way
+              you navigate technical documentation. Libersift leverages advanced
+              AI algorithms to deliver fast and accurate answers to your
+              queries.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
