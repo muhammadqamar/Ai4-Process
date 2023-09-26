b67e7a20452dffe4f1259fd129d1f5ecfe677a13
muhammadqamar <muhammadqamar111@gmail.com>
2023-09-26 16:42:06 +0500

    add header

--- src/components/Layout/Header/header.js
+++ src/components/Layout/Header/header.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Reqvise from "../../../assets/images/reqviseLogo2.svg"
-import { Button } from "../../../utils/Button"
+import { Button } from "../../../utils"
 
 const Header = () => {
   return (
-    <div className="w-full mx-auto">
+    <div className="w-full mx-auto flex justify-between mt-[25px]">
       <img src={Reqvise} alt="reqviseLogo" className="ml-[44px]" />
-      <div>
-        sdfsd
-        {/* <Button text="Support" />
-        <Button text="Logout" /> */}
+      <div className="flex flex-row gap-[14.3px] mr-[100px]">
+        <Button text="Support" color="text-Black" className="rounded-none h-[30px]" />
+        <Button text="Logout" bg='bg-Purple-500' color="text-Mist-500" className="h-[30px] rounded-none" />
       </div>
     </div>
   );
